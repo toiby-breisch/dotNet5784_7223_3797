efec6c6801170c2000ceb5bff38bdddffb7ba462
chayaBreisch <Handesaim3A5@mbjcomp.org.il>
2023-09-10 09:32:47 +0300

    a

---
+++ project0/project0/Program7223.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7223();
+            Welcome3797();
+            Console.ReadKey();
+        }
+        static partial void Welcome3797();
+        private static void Welcome7223()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console aplication", name);
+        }
+    }
+}
