1054635895d82f67555ea80e53cce175b529c202
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2023-10-18 01:01:01 +0300

    untill page 21

--- project0/project0/Program7223.cs
+++ project0/project0/Program7223.cs
@@ -3,36 +3,23 @@ using System.Xml.Linq;
 
 namespace project0
 {
-     class Program
+    partial class Program
     {
-        static void Main(string[] args)
-            
+         static void Main(string[] args)
+
         {
-           
-            
-            Console.Write("Enter your name:");
-            string userName = Console.ReadLine();
-            Console.WriteLine("{0},welcome to my first console aplication", userName);
+            Welcome7223();
+            Welcome3797();
             Console.ReadKey();
-            //Welcome7223();
-            //Welcome3797();
-            //Console.ReadKey();
-            //Console.WriteLine("Enter your name");
         }
 
-        //static partial void Welcome3797();
-        //private static void Welcome7223()
-        //{
-        //    Console.WriteLine("Enter your last name: ");
-        //    string name = Console.ReadLine();
-            
-        //}
+        static partial void Welcome3797();
+        private static void Welcome7223()
+        {
+            Console.WriteLine("Enter your  name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first consile application", name);
+
+        }
     }
 }
-//Console.WriteLine("Enter your name: ");
-//Console.ReadLine();
-
-
-//Console.WriteLine("{0},welcome to my first consile application");
-//Console.ReadKey();
-//Console.WriteLine("Press any key to continue...");
