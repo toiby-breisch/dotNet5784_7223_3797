b0f69c3e46c2530d6b5f3078db825491b306237d
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2023-10-17 15:34:30 +0300

    ghgjj

--- project0/project0/Program7223.cs
+++ project0/project0/Program7223.cs
@@ -1,22 +1,31 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace project0
 {
-    partial class Program
+     class Program
     {
         static void Main(string[] args)
+            
         {
-            Welcome7223();
-            Welcome3797();
+            
+            Console.Write("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console aplication", userName);
             Console.ReadKey();
+            //Welcome7223();
+            //Welcome3797();
+            //Console.ReadKey();
+            //Console.WriteLine("Enter your name");
         }
-        static partial void Welcome3797();
-        private static void Welcome7223()
-        {
-            Console.WriteLine("Enter your last name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("{0},welcome to my first console aplication", name);
-        }
+
+        //static partial void Welcome3797();
+        //private static void Welcome7223()
+        //{
+        //    Console.WriteLine("Enter your last name: ");
+        //    string name = Console.ReadLine();
+            
+        //}
     }
 }
 //Console.WriteLine("Enter your name: ");
