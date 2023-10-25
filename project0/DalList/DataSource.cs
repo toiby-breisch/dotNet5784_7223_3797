bfe75a8bcf4a0587c793d53c75bb9ade5c08d5db
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2023-10-25 15:44:22 +0300

    correct

--- project0/DalList/DataSource.cs
+++ project0/DalList/DataSource.cs
@@ -1,18 +1,12 @@
-﻿
-namespace Dal;
-using DO;
-internal static class DataSource
-{
-    internal static List<DO.Engineer> Engineers { get; } = new(40);
-    internal static List<DO.Task> Taskls { get; } = new();
-    internal static List<DO.Dependency> Dependencys { get; } = new();
-    internal static class Config
-    {
-        internal const int startTaskId = 1;
-        private static int nextTaskId = startTaskId;
-        internal static int NextTaskId { get => nextTaskId++; }
-        private static int nextDependencyId = startTaskId;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-        internal static int NextDependencyId { get => nextTaskId++; }
+namespace Dal
+{
+    internal class DataSource
+    {
     }
 }
