4f78f7355e68cb121130d752749ab93a500e5a88
chayaBreisch <Handesaim3A5@mbjcomp.org.il>
2023-10-28 23:22:27 +0300

    Main Menu

--- project0/DalTest/Initialization.cs
+++ project0/DalTest/Initialization.cs
@@ -1,36 +1,36 @@
-﻿namespace DalTest;
-using DalApi;
-using DO;
-using System;
-using System.Threading;
+﻿namespace DalTest {
+    using DalApi;
+    using DO;
+    using System;
+    using System.Threading;
 
-public static class Initialization
-{
-     
-    enum TaskLevel
+    public static class Initialization
     {
-         VERY_EASY , 
-        EASY, 
-        MEDIUM, 
-        HARD, 
-        VERY_HARD
-    }
 
-    private static IEngineer? s_dalEmployee; //stage 1
-    private static ITask? s_dalTask; //stage 1
-    private static IDependency? s_dalDependency; //stage 1
-
-    private static readonly Random s_rand = new();
-
-    private static void createEmployee()
-    {
-        const int MIN_ID = 200000000;
-        const int MAX_ID = 400000000;
-        const int MIN_C = 27;
-        const int MAX_C = 300;
-
-        string[] engineerNames =
+        enum TaskLevel
         {
+            VERY_EASY,
+            EASY,
+            MEDIUM,
+            HARD,
+            VERY_HARD
+        }
+
+        private static IEngineer? s_dalEmployee; //stage 1
+        private static ITask? s_dalTask; //stage 1
+        private static IDependency? s_dalDependency; //stage 1
+
+        private static readonly Random s_rand = new();
+
+        private static void createEmployee()
+        {
+            const int MIN_ID = 200000000;
+            const int MAX_ID = 400000000;
+            const int MIN_C = 27;
+            const int MAX_C = 300;
+
+            string[] engineerNames =
+            {
         "Dani Levi",
         "Eli Amar",
         "Yair Cohen",
@@ -77,82 +77,82 @@ public static class Initialization
 
     };
 
-        foreach (var _name in engineerNames)
-        {
-            int _id;
-            do
-                _id = s_rand.Next(MIN_ID, MAX_ID);
-            while (s_dalEngineer!.Read(_id) != null);
-            string _tEmail = _name;
-            string _tEmail.input.Trim().Replace(" ", "");
-            string _email = string(_tEmail + "@gmail.com");
-            EngineerExperience _level = s_rand.Next(1, 3);
-            int _cost = s_rand.Next(MIN_C, MAX_C);
-            Engineer newEngineer = new(_id, _name, _level, _email, _cost);
-            s_dalEmployee!.Create(newEngineer);
+            foreach (var _name in engineerNames)
+            {
+                int _id;
+                do
+                    _id = s_rand.Next(MIN_ID, MAX_ID);
+                while (s_dalEngineer!.Read(_id) != null);
+                string _tEmail = _name;
+                string _tEmail.input.Trim().Replace(" ", "");
+                string _email = string(_tEmail + "@gmail.com");
+                EngineerExperience _level = s_rand.Next(1, 3);
+                int _cost = s_rand.Next(MIN_C, MAX_C);
+                Engineer newEngineer = new(_id, _name, _level, _email, _cost);
+                s_dalEmployee!.Create(newEngineer);
+            }
         }
-    }
 
-    private static DateTime RandomDate(DateTime startDate, DateTime endDate)
-    {
-        Random gen = new Random();
-        int range = (endDate - startDate).Days;
-        return startDate.AddDays(gen.Next(range));
-    }
-
-    private static void createTask()
-    {
-        string[] Aliases = { "a", "b", "c", "d", "e" };
-           
-         for (int i = 0; i < 100; i++)
-         {
-            int indexA = s_rand.Next(0, 4);
-            int indexD = s_rand.Next(0, 4);
-            string _description = String(TaskLevel)indexD;
-            string _Alias = Aliases[indexA];
-            bool _Milestone = s_rand.Next(0, 1);
-            DateTime _CreatedAt = RandomDate(new DateTime(2020, 1, 1), DateTime.Today);
-            DateTime _Start = RandomDate(_CreatedAt, DateTime.Today);
-            DateTime ForecasDate = _Start.AddDays((((TaskLevel)indexD + 1) * 365).Days);
-            DateTime Deadline= ForecasDate.AddDays((365/2).Days);
-            DateTime Complete= RandomDate(ForecasDate, Deadline);
-            int indexEI = s_rand.Next(0, 40);
-            int _Engineerid = Engineers[indexEI].Id;
-            EngineerExperience _level = s_rand.Next(1, 3);
-            int indexR = s_rand.Next(0, 4);
-            int indexDe = s_rand.Next(0, 4);
-            string _Remarks = Remarks[indexR];
-            string _Deliverables = Deliverables[indexDe];
-            Task newTask = new(_Description, _Alias, _Milestone, _CreatedAt, _Start, _ForecasDate, _Deadline,
-            _Complete, _Deliverables, _Remarks, _Engineerid, _level);
-            s_dalTask!.Create(newTask);
-
-         }
-    }
-    private static void createDependency()
-    {
-        for(int i = 0; i < 100; i++)
+        private static DateTime RandomDate(DateTime startDate, DateTime endDate)
         {
-            int indexDT = s_rand.Next(0, 100);
-            int indexDOT = s_rand.Next(0, 100);
-            int _DependentTask = Dependencies[indexDT];
-            int _DependsOnTask = Dependencies[indexDOT];
-            Engineer newDependent = new(_DependentTask,_DependsOnTask);
-            s_dalDependency!.Create(newDependent);
+            Random gen = new Random();
+            int range = (endDate - startDate).Days;
+            return startDate.AddDays(gen.Next(range));
         }
-       
+
+        private static void createTask()
+        {
+            string[] Aliases = { "a", "b", "c", "d", "e" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                int indexA = s_rand.Next(0, 4);
+                int indexD = s_rand.Next(0, 4);
+                string _description = String(TaskLevel)indexD;
+                string _Alias = Aliases[indexA];
+                bool _Milestone = s_rand.Next(0, 1);
+                DateTime _CreatedAt = RandomDate(new DateTime(2020, 1, 1), DateTime.Today);
+                DateTime _Start = RandomDate(_CreatedAt, DateTime.Today);
+                DateTime ForecasDate = _Start.AddDays((((TaskLevel)indexD + 1) * 365).Days);
+                DateTime Deadline = ForecasDate.AddDays((365 / 2).Days);
+                DateTime Complete = RandomDate(ForecasDate, Deadline);
+                int indexEI = s_rand.Next(0, 40);
+                int _Engineerid = Engineers[indexEI].Id;
+                EngineerExperience _level = s_rand.Next(1, 3);
+                int indexR = s_rand.Next(0, 4);
+                int indexDe = s_rand.Next(0, 4);
+                string _Remarks = Remarks[indexR];
+                string _Deliverables = Deliverables[indexDe];
+                Task newTask = new(_Description, _Alias, _Milestone, _CreatedAt, _Start, _ForecasDate, _Deadline,
+                _Complete, _Deliverables, _Remarks, _Engineerid, _level);
+                s_dalTask!.Create(newTask);
+
+            }
+        }
+        private static void createDependency()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int indexDT = s_rand.Next(0, 100);
+                int indexDOT = s_rand.Next(0, 100);
+                int _DependentTask = Dependencies[indexDT];
+                int _DependsOnTask = Dependencies[indexDOT];
+                Engineer newDependent = new(_DependentTask, _DependsOnTask);
+                s_dalDependency!.Create(newDependent);
+            }
+
+        }
+
+
     }
-    
+    static public void Do(IEngineer? dalEngineer, ITask? dalTask, IDependency? dalDependency)
+    {
+        s_dalEngineer = dalEngineer ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalTask = dalTask ?? throw new NullReferenceException("DAL can not be null!");
+        s_dalDependency = dalDependency ?? throw new NullReferenceException("DAL can not be null!");
+        createEmployee();
+        createTask();
+        createDependency();
+    }
 
 }
- static public void  Do(IEngineer? dalEngineer,ITask? dalTask, IDependency? dalDependency)
-{
-    s_dalEngineer = dalEngineer ?? throw new NullReferenceException("DAL can not be null!");
-    s_dalTask = dalTask ?? throw new NullReferenceException("DAL can not be null!");
-    s_dalDependency = dalDependency ?? throw new NullReferenceException("DAL can not be null!");
-    createEmployee();
-    createTask();
-    reateDependency();
-}
-
-
--- project0/DalTest/Program.cs
+++ project0/DalTest/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace DalTest
+{
+    static public void Main_Menu(int num)
+    {
+        switch (num)
+        {
+            case 0:return;
+                break;
+                case 1:
+                createEmployee();
+                break;
+                case 2:
+                createTask();
+        }
+    }
+    internal class program
+    {
+        private static ITask? s_dalTask = new TaskImplementation();
+        private static IEngineer? s_dalEngineer = new EngineerImplementation();
+        private static IDependency? s_dalDependency = new DependencyImplementation();
+        static void Main()
+        {
+
+            try
+            {
+                Initialization.Do(s_dalTask, s_dalEngineer, s_dalDependency);
+
+            }
+            catch(Exception EX)
+            {
+                Console.WriteLine(EX);
+            }
+
+
+        }
+    }
+}
