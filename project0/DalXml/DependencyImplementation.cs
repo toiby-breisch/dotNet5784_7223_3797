3c5ad922f36a3158d702d24b32122573846ebf5c
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2023-11-16 23:20:44 +0200

    till page 7 in shlav 3

---
+++ project0/DalXml/DependencyImplementation.cs
@@ -0,0 +1,43 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+internal class DependencyImplementation : IDependency
+{
+    public int Create(Dependency item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool isDepend(int dependentTask, int dependsOnTask)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(Func<Dependency, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Dependency> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Dependency item)
+    {
+        throw new NotImplementedException();
+    }
+}
