efd3c0c859c762d8862da659c0ff3f82d4203813
chayaBreisch <Handesaim3A5@mbjcomp.org.il>
2023-11-20 15:53:29 +0200

    finished dependencyimplementation xml

--- project0/DalFacade/DalApi/IDependency.cs
+++ project0/DalFacade/DalApi/IDependency.cs
@@ -4,4 +4,5 @@ using DO;
 public interface IDependency : ICrud<Dependency>
 {
     public bool isDepend(int dependentTask, int dependsOnTask);//check if the task is depend
+   
 }
--- project0/DalXml/DependencyImplementation.cs
+++ project0/DalXml/DependencyImplementation.cs
@@ -8,47 +8,89 @@ internal class DependencyImplementation : IDependency
 {
 
 
-    public Dependency? Read(int _EngineerId, int _TaskId) //Reads entity object by 2 IDs
+    public bool isDepend(int dependentTask, int dependsOnTask) //Reads entity object by 2 IDs
     {
-        string fileName = "engineers";
-        List<Dependency>? dependencys = new List<Dependency>();
-        dependencys = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+        string fileName = "dependencies";
+        List<Dependency>? dependencies = new List<Dependency>();
+        dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+       
+        Dependency? isDepend= dependencies.Find(lk => lk.DependentTask == dependentTask && lk.DependsOnTask == dependsOnTask);
         return
-         dependencys.Find(lk => lk.DependentTask == _EngineerId && lk.DependsOnTask == _TaskId);
+            isDepend != null;
     }
 
         public int Create(Dependency item)
     {
-        throw new NotImplementedException();
+        string fileName = "dependencies";
+        List<Dependency>? dependencies = new List<Dependency>();
+        dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+        int newId =Config.NextDependencyId;
+        Dependency copy = item with { Id = newId };
+        dependencies.Add(copy);
+        XMLTools.SaveListToXMLSerializer<Dependency>(dependencies!, fileName);
+        return newId;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        string fileName = "dependencies";
+        List<Dependency>? dependencies = new List<Dependency>();
+        dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+        Dependency? result = dependencies.FirstOrDefault(dependency => dependency.Id == id);
+        if (result is not null)
+        {
+            dependencies.Remove(result);
+            XMLTools.SaveListToXMLSerializer<Dependency>(dependencies!, fileName);
+        }
+        else throw new DalDoesNotExistException($"Dependency with ID={id} is not exists");
     }
+    //public bool isDepend(int dependentTask, int dependsOnTask)
+    //{
+    //    throw new NotImplementedException();
+    //}
 
-    public bool isDepend(int dependentTask, int dependsOnTask)
-    {
-        throw new NotImplementedException();
-    }
 
     public Dependency? Read(int id)
     {
-        throw new NotImplementedException();
+        string fileName = "dependencies";
+        List<Dependency>? dependencies = new List<Dependency>();
+        dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+        Dependency? result = dependencies.FirstOrDefault(dependency => dependency.Id == id);
+        if (result is not null)
+            return result;
+        return null;
     }
 
     public Dependency? Read(Func<Dependency, bool> filter)
     {
-        throw new NotImplementedException();
+        string fileName = "dependencies";
+        List<Dependency>? dependencies = new List<Dependency>();
+        dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+        return dependencies.FirstOrDefault(d => filter(d));
     }
 
     public IEnumerable<Dependency> ReadAll(Func<Dependency, bool>? filter=null)
     {
-        throw new NotImplementedException();
+        string fileName = "dependencies";
+        List<Dependency>? dependencies = new List<Dependency>();
+        dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+        return new List<Dependency>(dependencies);
     }
 
     public void Update(Dependency item)
     {
-        throw new NotImplementedException();
-    }
+        string fileName = "dependencies";
+        List<Dependency>? dependencies = new List<Dependency>();
+        dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(fileName)!;
+        
+            Dependency? dependencyToUpdate = Read(item.Id);
+            if (dependencyToUpdate is not null)
+            {
+                dependencies.Remove(dependencyToUpdate);
+                Dependency dependency = new(item.Id, item.DependentTask, item.DependsOnTask);
+                dependencies.Add(dependency);
+            XMLTools.SaveListToXMLSerializer<Dependency>(dependencies!, fileName);
+        }
+            else throw new DalDoesNotExistException($"Dependency with ID={item.Id} does not exists ");
+        }
 }
