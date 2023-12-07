ee975f1390d2d98366cfc8cb48d2428f9a708b7a
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2023-12-07 21:20:03 +0200

    conntinue

--- project0/BL/BO/Engineer.cs
+++ project0/BL/BO/Engineer.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DO;
 
-namespace BO
+namespace BO;
+public class Student
 {
-    internal class Engineer
-    {
-    }
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public int? Cost { get; set; }
+    public EngineerExperience Level { get; set; }
+    public TaskInEngineer Task { get; set; }
+  //  public override string ToString() => this.ToStringProperty();
 }
