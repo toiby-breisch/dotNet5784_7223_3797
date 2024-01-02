9dfb5847eba6e4ba015f0702822f63d28b0eaff7
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2024-01-03 00:13:34 +0200

    btnEngineers_Click

--- project0/pl/MainWindow.xaml.cs
+++ project0/pl/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using pl.Engineer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace pl
         public MainWindow()
         {
             InitializeComponent();
+           
+        }
+        private void btnEngineers_Click(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
         }
     }
 }
