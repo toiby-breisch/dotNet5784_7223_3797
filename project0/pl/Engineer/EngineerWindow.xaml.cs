eaa4dc104007c38b45a0ea7ec813a7b16fe9c50a
ברייש טויבי < @B21>
2024-01-04 13:56:57 +0200

    try again

--- project0/pl/Engineer/EngineerWindow.xaml.cs
+++ project0/pl/Engineer/EngineerWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,15 +21,11 @@ namespace pl.Engineer
     public partial class EngineerWindow : Window
     {
         private static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-        public int ID
-        {//לשאול
-            get { return (int)GetValue(IDProperty); }
-            set { SetValue(IDProperty, value); }
-        }
 
+        
         public BO.Engineer CurrentEngineer
         {
-
+          
             get { return (BO.Engineer)GetValue(CurrentEngineerProperty); }
             set { SetValue(CurrentEngineerProperty, value); }
         }
@@ -36,15 +33,28 @@ namespace pl.Engineer
         // Using a DependencyProperty as the backing store for EngineersValue.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CurrentEngineerProperty =
             DependencyProperty.Register("EngineersValue", typeof(BO.Engineer), typeof(BO.Engineer), new PropertyMetadata(0));
-        //לשאול
-        public static readonly DependencyProperty IDProperty =
-          DependencyProperty.Register("EngineersValue", typeof(int), typeof(int), new PropertyMetadata(0));
 
 
-        public EngineerWindow()
+        public EngineerWindow(int Id = 0)
         {
+           
             InitializeComponent();
-
-        }
+            if (Id == 0)
+            {
+                BO.Engineer Engineer =  new BO.Engineer {Id=0,Name="", Email="", Cost=0, Level=BO.EngineerExperience.None };
+            }
+            else
+            {
+                try
+                {
+                    BO.Engineer newEngineer = s_bl.Engineer.Read(Id)!;
+                }
+                ////////לשאול!!!!!!!!!!!!!!!!!!!!!!dont forget!!!!!!!!!!!!please!!!!!!forever!to smile;)
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        
     }
 }
