e1f38bdf49763c308d338aa6e935867a8cc0edb1
רחל שרשבסקי <Hand4c35@mbjcomp.org.il>
2024-02-11 21:18:54 +0200

    create tag for shlav5!

--- project0/BL/BO/Engineer.cs
+++ project0/BL/BO/Engineer.cs
@@ -5,18 +5,12 @@ namespace BO;
 public class Engineer
 {
     public int Id { get; init; }
-
     public required string Name { get; set; }
     public required string Email { get; set; }
     public double Cost { get; set; }
     public EngineerExperience Level { get; set; }
-    public TaskInEngineer? CurrentTask { get; set; }
-  
+    public TaskInEngineer? CurrentTask { get; set; } 
    public override string ToString() => Tools.ToStringProperty(this);
-
-
-
-
 }
 
 
